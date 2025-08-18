86551becbb78c9a8a57018be796954bb67c330be
mateuscaetano952 <mateuscaetano952@gmail.com>
2025-08-18 10:56:29 -0300

    configurations

--- serve.js
+++ serve.js
@@ -4,18 +4,64 @@ const app = express();
 const path = require('path');
 const routes = require('./routes');
 const conn = require('./src/db/conn');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('connect-flash');
 
+//handlebars setup
 const { engine } = require('express-handlebars');
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
+//static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Session middleware
+app.use(session({
+  name: "session",
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore(),
+  path: path.join(require('os').tmpdir(), 'sessions'),
+  cookie: {
+    secure:false,
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 day
+    httppOnly: true
+  }
+}));
+
+//Flash messages middleware
+app.use(flash());
+
+//Set session to res
+app.use((req, res, next) => {
+  
+  if(req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
+//routes
 app.use('/', routes);
 
+//server connection
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+conn
+.sync()
+.then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+}).catch((err) => {
+  console.error('Failed to connect to the database:', err);
 });
